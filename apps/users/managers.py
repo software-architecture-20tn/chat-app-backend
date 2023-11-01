be1388b804a4478f4d089a98dc8c22346f38f4f1
Nguyen Van Loc <vanloc1808@gmail.com>
2023-11-01 21:13:54 +0700

    CHAT-15 Update user creation

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -6,22 +6,22 @@ from django.contrib.auth.models import UserManager as DjangoUserManager
 class UserManager(DjangoUserManager):
     """Custom manager for the User model."""
 
-    def _create_user(self, email: str, password: str | None, **extra_fields):
+    def _create_user(self, email: str, username: str, password: str | None, **extra_fields):
         """
         Create and save a user with the given email and password.
         """
         if not email:
             raise ValueError("The given email must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email: str, password: str | None = None, **extra_fields):
+    def create_user(self, email: str, username: str, password: str | None = None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, username, password, **extra_fields)
 
     def create_superuser(self, email: str, password: str | None = None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
@@ -31,5 +31,10 @@ class UserManager(DjangoUserManager):
             raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
-
-        return self._create_user(email, password, **extra_fields)
+        username = email
+        return self._create_user(
+            email=email,
+            password=password,
+            username=username,
+            **extra_fields,
+        )
