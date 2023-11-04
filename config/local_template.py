2ef301ba36ca02dc033db1ac9354fc5d55788ae3
Nguyen Van Loc <vanloc1808@gmail.com>
2023-11-04 10:10:36 +0700

    CHAT-26 Fix typo

--- config/local_template.py
+++ config/local_template.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don"t run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ROOT_URLCONF = "config.urls"
