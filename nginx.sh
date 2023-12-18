34a813c71db898db032b6773c686289dc802885c
Nguyen Van Loc <vanloc1808@gmail.com>
2023-12-18 23:15:32 +0700

    Add app.conf

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/sites-available/app
+sudo cp -rf app.conf /etc/nginx/sites-available/chat-app.nguyenvanloc.name.vn
 sudo chmod 710 /var/lib/jenkins/workspace/chat-app-backend
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/chat-app.nguyenvanloc.name.vn /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
