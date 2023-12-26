20aac4158b369e68fe2aa69ab7857480845f9c95
Nguyen Van Loc <vanloc1808@gmail.com>
2023-12-26 14:41:14 +0700

    Integrate Sentry

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.local_template")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.dev_config")
 
 application = get_wsgi_application()
